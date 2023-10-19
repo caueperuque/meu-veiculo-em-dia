0a071c282aabbd728335c4fcf427875f699fa3ba
caue peruque <caue19052003@gmail.com>
2023-10-19 12:21:12 -0300

    fix: signup

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -1,16 +1,19 @@
 import './MainHeader.css';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Context from '../../context/Context';
 
 
 
 export default function MainHeader() {
+  const [typeUser, setTypeUser] = useState();
+
   const { userName, setUserName } = useContext(Context)
   useEffect(() => {
     const login = JSON.parse(localStorage.getItem('login'));
     const splitName = login.name.split(" ");
+    setTypeUser(login.type);
     setUserName(splitName[0]);
   })
   return (
@@ -23,9 +26,15 @@ export default function MainHeader() {
           <Link to="/register" className="header__btn-signup">
             teste
           </Link>
-          <Link to="/register" className="header__btn-login">
-            Cadastrar Produto
-          </Link>
+          { typeUser === "cliente" ? (
+            <Link to="/register" className="header__btn-login">
+              Sou cliente
+            </Link>
+          ) : (
+            <Link to="/register" className="header__btn-login">
+              Cadastrar Produto
+            </Link>
+          ) }
         </div>
       </nav>
     </header>
